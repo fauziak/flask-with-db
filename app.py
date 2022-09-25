65defc08ab4f587d09934abfb83df668677b3b65
fauziak <fauzia.kolia@stonybrook.edu>
2022-09-25 17:57:36 -0400

    updated app.py

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def index():
   return render_template("patients_table.html", patients=patients)
 
 if __name__ == "__main__":
-  app.run(HOST_NAME, HOST_PORT)
+  app.run(HOST_NAME, HOST_PORT, True)
   
